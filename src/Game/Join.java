c3e4abd08416104938f429028134d1858c73f5fa
mc1128 <mc1128@naver.com>
2021-04-09 09:46:11 +0900

    Join 레이아웃 위치수정

--- src/Game/Join.java
+++ src/Game/Join.java
@@ -43,50 +43,47 @@ public class Join extends JFrame {
 
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Join.class.getResource("/Game/image/joinback.png")));
 		setTitle("회원가입 화면 텟");
-
+				
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 400, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
+		
 		JLabel Panel_Join = new JLabel("회원가입");
-		Panel_Join.setFont(new Font("맑은 고딕", Font.PLAIN, 36));
+		Panel_Join.setFont(new Font("맑은 고딕", Font.BOLD, 36));
 		Panel_Join.setHorizontalAlignment(SwingConstants.CENTER);
-		Panel_Join.setBounds(114, 51, 160, 42);
+		Panel_Join.setBounds(114, 79, 160, 42);
 		contentPane.add(Panel_Join);
-
+		
 		ID_Join = new JTextField();
-		ID_Join.setBounds(95, 160, 200, 25);
+		ID_Join.setBounds(103, 160, 172, 25);
 		contentPane.add(ID_Join);
 		ID_Join.setColumns(10);
-
+		
 		PW_Join = new JPasswordField();
-		PW_Join.setEchoChar('*');
-		PW_Join.setBounds(95, 220, 200, 25);
+		PW_Join.setBounds(103, 214, 171, 25);
 		contentPane.add(PW_Join);
 		PW_Join.setColumns(10);
-
+		
 		btnNewButton = new JButton("완료");
-		btnNewButton.setBounds(130, 300, 125, 35);
+		btnNewButton.setBounds(129, 280, 125, 35);
 		contentPane.add(btnNewButton);
-
+		
 		JLabel lblNewLabel_1 = new JLabel("ID");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(165, 140, 57, 15);
 		contentPane.add(lblNewLabel_1);
-
+		
 		JLabel lblNewLabel_2 = new JLabel("Password");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(135, 200, 115, 15);
+		lblNewLabel_2.setBounds(139, 195, 115, 15);
 		contentPane.add(lblNewLabel_2);
-
-		setVisible(true); // GUI 최하단에 두기
-
-		// 이하 배경화면
-		String path = "";
-
+		
+		// 이하 배경화면 
+		String path="";
+		
 		try { // path 설정
 			path = URLDecoder.decode(Game_Screen1.class.getResource("").getPath(), "UTF-8");
 		} catch (UnsupportedEncodingException e1) {
@@ -100,6 +97,8 @@ public class Join extends JFrame {
 		join_back.setLocation(0, 0);
 		join_back.setSize(384, 361);
 		contentPane.add(join_back);
+		
+		setVisible(true);	// GUI 최하단에 두기
 
 		btnNewButton.addActionListener(new ActionListener() {
 
