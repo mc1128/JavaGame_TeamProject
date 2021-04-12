89aa8a32a41c874c972ad179e49e87b88c53aa21
csooy <csooy38@gmail.com>
2021-04-12 14:07:56 +0900

    Profile GUI 변경

--- src/Game/Profile.java
+++ src/Game/Profile.java
@@ -4,113 +4,119 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.text.DecimalFormat;
 
 import javax.swing.*;
 
-
 public class Profile extends JFrame {
+	private final JLabel back = new JLabel("New label");
 
+	String path;
 
-	
 	public Profile() {
-		getContentPane().setForeground(new Color(0, 0, 0));
-		getContentPane().setBackground(new Color(255, 239, 213));
+//		getContentPane().setForeground(new Color(0, 0, 0));
+//		getContentPane().setBackground(new Color(255, 239, 213));
 
 		setTitle("유저 프로필");
 
 		getContentPane().setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("유저 프로필");
-		lblNewLabel.setForeground(new Color(0, 0, 0));
-		lblNewLabel.setBounds(244, 27, 100, 30);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(224, 27, 150, 30);
 		getContentPane().add(lblNewLabel);
-		lblNewLabel.setOpaque(true);
-		lblNewLabel.setBackground(new Color(211, 211, 211));
-		lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 18));
-
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(210, 180, 140));
-		panel.setBounds(50, 40, 500, 300);
-		getContentPane().add(panel);
-		panel.setLayout(null);
-
-		JLabel lbName = new JLabel("닉네임 : " + Values.name);
-		lbName.setForeground(new Color(0, 0, 0));
-		lbName.setBackground(new Color(211, 211, 211));
-		lbName.setOpaque(true);
-		lbName.setBounds(123, 39, 111, 25);
-		panel.add(lbName);
+//		lblNewLabel.setOpaque(true);
+//		lblNewLabel.setBackground(new Color(211, 211, 211));
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 20));
 
 		JLabel lbGold = new JLabel("보유 골드 : " + Values.gold_save);
-		lbGold.setForeground(new Color(0, 0, 0));
-		lbGold.setBounds(123, 74, 111, 25);
-		lbGold.setOpaque(true);
-		panel.add(lbGold);
-		lbGold.setBackground(new Color(211, 211, 211));
+		lbGold.setBounds(171, 109, 111, 25);
+		getContentPane().add(lbGold);
+//		lbGold.setOpaque(true);
+		// lbGold.setBackground(new Color(211, 211, 211));
 
 		JLabel lbWin = new JLabel("승 : " + Values.user_win);
-		lbWin.setBackground(new Color(211, 211, 211));
-		lbWin.setForeground(new Color(0, 0, 0));
-		lbWin.setBounds(123, 109, 111, 25);
-		lbWin.setOpaque(true);
-		panel.add(lbWin);
+		lbWin.setBounds(171, 131, 111, 25);
+		getContentPane().add(lbWin);
+//		lbWin.setOpaque(true);
 
 		JLabel lbDraw = new JLabel("무 : " + Values.user_draw);
-		lbDraw.setForeground(new Color(0, 0, 0));
-		lbDraw.setBackground(new Color(211, 211, 211));
-		lbDraw.setBounds(123, 144, 111, 25);
-		lbDraw.setOpaque(true);
-		panel.add(lbDraw);
+		lbDraw.setBounds(171, 154, 111, 25);
+		getContentPane().add(lbDraw);
+//		lbDraw.setOpaque(true);
 
 		JLabel defeat = new JLabel("패 : " + Values.user_defeat);
-		defeat.setForeground(new Color(0, 0, 0));
-		defeat.setBackground(new Color(211, 211, 211));
-		defeat.setBounds(123, 179, 111, 25);
-		defeat.setOpaque(true);
-		panel.add(defeat);
-
-		
+		defeat.setBounds(171, 177, 111, 25);
+		getContentPane().add(defeat);
+//		defeat.setOpaque(true);
 		
 		int total = Values.user_win + Values.user_defeat + Values.user_draw;
-		double rate = (double)((double)Values.user_win / (double)total) * 100.0;
-		if(Double.isNaN(rate)) {
-			rate = (double)(0.00);
+		double rate = (double) ((double) Values.user_win / (double) total) * 100.0;
+		if (Double.isNaN(rate)) {
+			rate = (double) (0.00);
 		}
 		String dispPattern = "0.##";
 		DecimalFormat form = new DecimalFormat(dispPattern);
 		String winning_rate = form.format(rate) + "%";
+		
 		JLabel lbWinRate = new JLabel("승률 : " + winning_rate);
-		lbWinRate.setBackground(new Color(211, 211, 211));
-		lbWinRate.setForeground(new Color(0, 0, 0));
-		lbWinRate.setBounds(123, 214, 111, 25);
-		lbWinRate.setOpaque(true);
-		panel.add(lbWinRate);
-		
-		
-		JButton startGame = new JButton("게임 시작");
-		startGame.setBounds(180, 250, 120, 35);
-		panel.add(startGame);
+		lbWinRate.setBounds(171, 212, 111, 25);
+		getContentPane().add(lbWinRate);
+//		lbWinRate.setBackground(new Color(211, 211, 211));
+//		lbWinRate.setForeground(new Color(0, 0, 0));
+//		lbWinRate.setOpaque(true);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setVisible(true);
+		try { // path 설정
+			path = URLDecoder.decode(Game_Screen1.class.getResource("").getPath(), "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			System.out.println("경로설정 오류");
+		}
+		;
 
-		setBounds(100, 100, 600, 400);
+		String back_path = path + "image/p_parch.png";
+		String frame_path = path + "image/p_frame.png";
+
+		JLabel lbName = new JLabel("닉네임 : " + Values.name);
+		lbName.setBounds(171, 85, 111, 25);
+		getContentPane().add(lbName);
+		// lbName.setForeground(new Color(0, 0, 0));
+		// lbName.setBackground(new Color(211, 211, 211));
+//		lbName.setOpaque(true);
+
+		JButton startGame = new JButton("게임 시작");
+		startGame.setBounds(243, 291, 120, 35);
+		getContentPane().add(startGame);
 		
+		JLabel frame = new JLabel(new ImageIcon(frame_path));
+		frame.setBounds(324, 67, 150, 214);
+		getContentPane().add(frame);
+
+		// 배경 이미지
+
+		JLabel back_1 = new JLabel(new ImageIcon(back_path));
+		back_1.setBounds(0, 0, 584, 361);
+		getContentPane().add(back_1);
+
 		startGame.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
 				new Batting();
 			}
 		});
-	}
 
+		setBounds(100, 100, 600, 400);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		new Profile();
-		
+
 	}
 }
